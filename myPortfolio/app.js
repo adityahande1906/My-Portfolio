df74e7edefac48c044f0dcb9a914d4b1e4661362
adityahande1906 <154577330+adityahande1906@users.noreply.github.com>
2024-01-18 20:10:07 +0530

    Add files via upload

---
+++ myPortfolio/app.js
@@ -0,0 +1,8 @@
+/* ===================typing animaton */
+
+var typed  = new Typed(".typing",{
+    strings:["Web Developer" , "Web Designer" , "Front End Developer" , "Back End Developer" , "Full Stack Web Developer" , "Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
